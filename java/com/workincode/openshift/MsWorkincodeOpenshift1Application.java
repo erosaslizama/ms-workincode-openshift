f65764bd78fac93952c56fef549008fc0083977a
erosas <erosas@DESKTOP-ND0581B>
2021-07-20 23:28:27 -0400

    init commit

---
+++ java/com/workincode/openshift/MsWorkincodeOpenshift1Application.java
@@ -0,0 +1,13 @@
+package com.workincode.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWorkincodeOpenshift1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsWorkincodeOpenshift1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/workincode/openshift/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workincode.openshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
